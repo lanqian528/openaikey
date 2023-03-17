2cdaf4422b7ed90c70c0e2174fedd558b3372c39
LanQian <877133467@qq.com5499636+chenjunyan@users.noreply.github.com>
2023-03-17 16:08:47 +0800

    修改样式

---
+++ script.js
@@ -0,0 +1,47 @@
+function sendCurlRequest() {
+    const apiUrl = 'https://openai.lan-qian.top/';
+    const apiKey = document.getElementById('apiKeyInput').value;
+    const url = `${apiUrl}/dashboard/billing/credit_grants`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${apiKey}`
+        }
+    };
+    fetch(url, options)
+        .then(response => response.json())
+        .then(responseJson => displayResult(responseJson))
+        .catch(error => console.error(error));
+}
+
+function displayResult(result) {
+    const totalGrantedElement = document.getElementById('totalGranted');
+    const totalUsedElement = document.getElementById('totalUsed');
+    const totalAvailableElement = document.getElementById('totalAvailable');
+    const effectiveAtElement = document.getElementById('effectiveAt');
+    const expiresAtElement = document.getElementById('expiresAt');
+
+    totalGrantedElement.innerText = result.total_granted;
+    totalUsedElement.innerText = result.total_used;
+    totalAvailableElement.innerText = result.total_available;
+    effectiveAtElement.innerText = formatDate(result.grants.data[0].effective_at) + ' 开始';
+    expiresAtElement.innerText = formatDate(result.grants.data[0].expires_at) + ' 结束';
+
+    const resultSection = document.getElementById('resultSection');
+    resultSection.style.display = 'block';
+}
+
+function formatDate(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const year = date.getFullYear();
+    const month = addLeadingZero(date.getMonth() + 1);
+    const day = addLeadingZero(date.getDate());
+    const hour = addLeadingZero(date.getHours());
+    const minute = addLeadingZero(date.getMinutes());
+    const second = addLeadingZero(date.getSeconds());
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+function addLeadingZero(number) {
+    return number < 10 ? '0' + number : number;
+}

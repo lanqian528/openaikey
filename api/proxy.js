e7a88efa931e50a66bade0fea65f3064b616f15b
LanQian <877133467@qq.com5499636+chenjunyan@users.noreply.github.com>
2023-08-09 21:59:16 +0800

    todo:新增sess查询

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,49 @@
-const request = require('request');
+// const request = require('request');
+//
+// module.exports = (req, res) => {
+//     // proxy middleware options
+//     let prefix = "/fakeopen"
+//     if (!req.url.startsWith(prefix)) {
+//         return;
+//     }
+//     let target = "https://ai.fakeopen.com" + req.url.substring(prefix.length);
+//
+//     var options = {
+//         url: target,
+//         method: req.method,
+//         headers: req.headers,
+//         body: req.body
+//     };
+//     request(options, function (error, response) {
+//         if (error) throw new Error(error);
+//         res.writeHead(200, {"Content-Type": "application/json"});
+//         res.write(response.body);
+//         res.end();
+//     });
+// }
+const axios = require('axios');
 
-module.exports = (req, res) => {
-    // proxy middleware options
-    let prefix = "/fakeopen"
+module.exports = async (req, res) => {
+    const prefix = "/fakeopen";
     if (!req.url.startsWith(prefix)) {
         return;
     }
-    let target = "https://ai.fakeopen.com" + req.url.substring(prefix.length);
+    const targetUrl = "https://ai.fakeopen.com" + req.url.substring(prefix.length);
 
-    var options = {
-        url: target,
-        method: req.method,
-        headers: req.headers,
-        body: req.body
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        res.writeHead(200, {"Content-Type": "application/json"});
-        res.write(response.body);
-        res.end();
-    });
-}
+    try {
+        const response = await axios({
+            method: req.method,
+            url: targetUrl,
+            headers: req.headers,
+            data: req.body,
+        });
+
+        res.status(response.status);
+        res.setHeader('Content-Type', response.headers['content-type']);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
